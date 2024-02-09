e418e07c0e076993983ffc8f5d6a1567f4845b58
Dhakshin A V <avdhakshin1354@gmail.com>
2024-02-10 00:19:43 +0530

    Basic interface

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function ToggleTheme() {
+    var body = document.querySelector('body');
+    var theme = document.getElementById("theme");
+    var logo = document.getElementById("logo");
+    if (body.classList.contains('night')) {
+        body.classList.remove('night');
+        theme.classList.remove('fa-sun');
+        theme.classList.add('fa-moon');
+        logo.src = 'img/logo-light.png';
+    } else {
+        body.classList.add('night');
+        theme.classList.remove('fa-moon');
+        theme.classList.add('fa-sun');
+        logo.src = 'img/logo-night.png'; 
+        }
+}
